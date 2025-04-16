43c3213bcfb2ada4e8b0dc788e1c7c92e3e4af89
Opa Maj <saintkilla79@gmail.com>
2025-04-16 10:13:02 +0300

    Добавьте файлы проекта.

---
+++ MassMedia/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MassMedia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

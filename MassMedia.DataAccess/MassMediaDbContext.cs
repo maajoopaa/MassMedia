184689e4aaee8cee59a443233f29c2f8c6b558f8
Opa Maj <saintkilla79@gmail.com>
2025-05-10 23:04:25 +0300

    Db

---
+++ MassMedia.DataAccess/MassMediaDbContext.cs
@@ -0,0 +1,33 @@
+﻿using MassMedia.DataAccess.Configurations;
+using MassMedia.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassMedia.DataAccess
+{
+    public class MassMediaDbContext  : DbContext
+    {
+        public MassMediaDbContext(DbContextOptions<MassMediaDbContext> options)
+            : base(options) { }
+
+        public DbSet<UserEntity> Users { get; set; }
+
+        public DbSet<ArticleEntity> Articles { get; set; }
+
+        public DbSet<MassMediaEntity> MassMedias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UserConfiguration());
+            modelBuilder.ApplyConfiguration(new ArticleConfiguration());
+            modelBuilder.ApplyConfiguration(new MassMediaConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
